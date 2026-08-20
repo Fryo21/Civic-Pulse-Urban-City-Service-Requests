b7224d0551a4d3cef2d7020be748aa6836cb7eb1
Fryo21 <fobiokala@gmail.com>
2026-08-20 11:06:26 +0100

    feat: converted the crime ingestion Azure Function to use Durable Function with an HTTP starter, orcherstrator and activity function

--- function_app.py
+++ function_app.py
@@ -1,43 +1,42 @@
 import json
 import azure.functions as func
-
+import azure.durable_functions as df
 from src.main import run_ingestion
 
 
-app = func.FunctionApp()
+app = df.DFApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.route(
-    route="ingest-crime",
-    methods=["POST"],
-    auth_level=func.AuthLevel.FUNCTION
-)
-def ingest_crime(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="ingest-crime", methods=["POST"])
+
+@app.durable_client_input(client_name="client")
+
+
+
+async def ingest_crime(req: func.HttpRequest, client: df.DurableOrchestrationClient) -> func.HttpResponse:
 
     try:
+
         body = req.get_json()
 
-        force = body.get("force")
+        force = body.get("force_name")
+
         year = body.get("year")
+
         month = body.get("month")
 
         if not force or not year or not month:
-            return func.HttpResponse(
-                "force, year and month are required.",
-                status_code=400
-            )
 
-        result = run_ingestion(
-            force,
-            year,
-            month
-        )
+            return func.HttpResponse("force, year and month are required.", status_code=400)
 
-        return func.HttpResponse(
-            json.dumps(result),
-            mimetype="application/json",
-            status_code=200
-        )
+        input_data = {"force_name": force,
+                       "year": year, 
+                       "month": month}
+
+        instance_id = await client.start_new("crime_ingestion_orchestrator", client_input=input_data)
+
+        return client.create_check_status_response(req, instance_id)
+    
 
     except Exception as error:
 
@@ -45,3 +44,27 @@ def ingest_crime(req: func.HttpRequest) -> func.HttpResponse:
             str(error),
             status_code=500
         )
+
+
+@app.orchestration_trigger(context_name="context")
+
+def crime_ingestion_orchestrator(context: df.DurableOrchestrationContext):
+
+    input_data = context.get_input()
+
+    result = yield context.call_activity("crime_ingestion_activity", input_data)
+
+    return result
+
+
+@app.activity_trigger(input_name="input_data")
+
+def crime_ingestion_activity(input_data: dict):
+
+    force_name = input_data["force_name"]
+
+    year = input_data["year"]
+
+    month = input_data["month"]
+
+    return run_ingestion(force_name, year, month)
