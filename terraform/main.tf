b73c7fcc1611bd7dca5fce8c718533f1cb930323
Fryo21 <fobiokala@gmail.com>
2026-08-24 13:19:25 +0100

    fix: correct Flex Function App Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,50 +1,68 @@
 terraform {
+
   required_providers {
+
     azurerm = {
+
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+
     }
+
   }
 
   backend "azurerm" {
+
     resource_group_name  = "rg-london-crime-data"
     storage_account_name = "teraformstate1212"
     container_name       = "tfstate"
     key                  = "london-crime.tfstate"
     use_azuread_auth     = true
+
   }
+
 }
 
 provider "azurerm" {
+
   features {}
+
   storage_use_azuread = true
+
 }
 
 module "resource_group" {
+
   source = "./modules/resource-group"
 
   name     = var.resource_group_name
   location = var.location
+
 }
 
 module "storage" {
+
   source = "./modules/storage"
 
   resource_group_name = module.resource_group.name
   location            = var.location
   primary_name        = var.storage_primary_name
   additional_name     = var.storage_additional_name
+
 }
 
 module "data_factory" {
+
   source = "./modules/data-factory"
 
   resource_group_name = module.resource_group.name
   location            = var.location
   name                = var.data_factory_name
+
 }
 
 module "postgres" {
+
   source = "./modules/postgres"
 
   resource_group_name    = module.resource_group.name
@@ -54,22 +72,27 @@ module "postgres" {
   administrator_password = var.postgressql_admin_password
   sku_name               = var.postgres_sku_name
   storage_mb             = var.postgres_storage_mb
+
 }
 
 module "function_app" {
+
   source = "./modules/function-app"
 
-  resource_group_name  = module.resource_group.name
-  location             = var.location
-  name                 = var.function_app_name
-  service_plan_name    = var.function_app_service_plan_name
-  storage_account_name = module.storage.additional_name
+  resource_group_name           = module.resource_group.name
+  location                      = var.location
+  name                          = var.function_app_name
+  service_plan_name             = var.function_app_service_plan_name
+  deployment_container_endpoint = module.storage.deployment_container_endpoint
+
+  host_storage_account_id = module.storage.additional_id
+  data_storage_account_id = module.storage.primary_id
 
   app_settings = {
-    "AzureWebJobsStorage__blobServiceUri"  = "https://${var.storage_additional_name}.blob.core.windows.net"
-    "AzureWebJobsStorage__credential"      = "managedidentity"
-    "AzureWebJobsStorage__queueServiceUri" = "https://${var.storage_additional_name}.queue.core.windows.net"
-    "AzureWebJobsStorage__tableServiceUri" = "https://${var.storage_additional_name}.table.core.windows.net"
+
+    "AzureWebJobsStorage"              = ""
+    "AzureWebJobsStorage__accountName" = module.storage.additional_name
+    "FUNCTIONS_EXTENSION_VERSION"      = "~4"
 
     "BRONZE_CONTAINER_NAME"      = "bronze-london"
     "QUARANTINE_CONTAINER_NAME"  = "quarantine-london"
@@ -79,13 +102,17 @@ module "function_app" {
     "URL_NEIGHBOURHOOD_BOUNDARY" = "https://data.police.uk/api/{force_id}/{neighbourhood_id}/boundary"
     "URL_POLICE_FORCE"           = "https://data.police.uk/api/forces"
     "URL_STREET_LEVEL_CRIME"     = "https://data.police.uk/api/crimes-street/all-crime"
+
   }
+
 }
 
 module "monitoring" {
+
   source = "./modules/monitoring"
 
   resource_group_name = module.resource_group.name
   data_factory_id     = module.data_factory.id
   email_address       = var.monitor_email_address
+
 }
--- terraform/modules/function-app/main.tf
+++ terraform/modules/function-app/main.tf
@@ -1,45 +1,108 @@
 resource "azurerm_service_plan" "this" {
+
   name                = var.service_plan_name
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Linux"
   sku_name            = "FC1"
+
 }
 
-resource "azurerm_linux_function_app" "this" {
+resource "azurerm_function_app_flex_consumption" "this" {
+
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
+  service_plan_id     = azurerm_service_plan.this.id
 
-  storage_account_name = var.storage_account_name
-  service_plan_id      = azurerm_service_plan.this.id
+  storage_container_type      = "blobContainer"
+  storage_container_endpoint  = var.deployment_container_endpoint
+  storage_authentication_type = "SystemAssignedIdentity"
 
-  functions_extension_version = "~4"
-  builtin_logging_enabled     = false
-  client_certificate_mode     = "Required"
-  https_only                  = true
+  runtime_name    = "python"
+  runtime_version = "3.10"
+
+  maximum_instance_count = 100
+  instance_memory_in_mb  = 2048
+
+  https_only              = true
+  client_certificate_mode = "Required"
 
   app_settings = var.app_settings
 
   sticky_settings {
+
     app_setting_names = keys(var.app_settings)
-  }
 
-  site_config {
-    ftps_state                        = "FtpsOnly"
-    ip_restriction_default_action     = "Allow"
-    scm_ip_restriction_default_action = "Allow"
-
-    cors {
-      allowed_origins = ["https://portal.azure.com"]
-    }
   }
 
   identity {
+
     type = "SystemAssigned"
+
   }
+
+  site_config {
+
+    cors {
+
+      allowed_origins = ["https://portal.azure.com"]
+
+    }
+
+  }
+
+}
+
+resource "azurerm_role_assignment" "host_blob" {
+
+  scope = replace(
+    var.host_storage_account_id,
+    "resourceGroups",
+    "resourcegroups"
+  )
+
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_function_app_flex_consumption.this.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+
+}
+
+resource "azurerm_role_assignment" "host_queue" {
+
+  scope                = var.host_storage_account_id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_function_app_flex_consumption.this.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+
+}
+
+resource "azurerm_role_assignment" "host_table" {
+
+  scope                = var.host_storage_account_id
+  role_definition_name = "Storage Table Data Contributor"
+  principal_id         = azurerm_function_app_flex_consumption.this.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+
+}
+
+resource "azurerm_role_assignment" "data_storage" {
+
+  scope                = var.data_storage_account_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_function_app_flex_consumption.this.identity[0].principal_id
+  principal_type       = "ServicePrincipal"
+
 }
 
 output "id" {
-  value = azurerm_linux_function_app.this.id
+
+  value = azurerm_function_app_flex_consumption.this.id
+
+}
+
+output "principal_id" {
+
+  value = azurerm_function_app_flex_consumption.this.identity[0].principal_id
+
 }
